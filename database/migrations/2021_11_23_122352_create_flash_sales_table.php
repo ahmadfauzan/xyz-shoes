ca6a2f8524ef537871d645c310160b67a5ffa7d3
ahmadfauzan <fauzana566@gmail.com>
2021-11-23 20:18:34 +0700

    updated migrations

---
+++ database/migrations/2021_11_23_122352_create_flash_sales_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFlashSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flash_sales', function (Blueprint $table) {
+            $table->id();
+            $table->integer('discounts_id');
+            $table->dateTime('start_at');
+            $table->dateTime('finish_at');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flash_sales');
+    }
+}
---
+++ database/migrations/2021_11_23_131228_create_detail_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('users_id');
+            $table->integer('address_id');
+            $table->integer('orders_id');
+            $table->integer('total_shipping');
+            $table->integer('total_price');
+            $table->boolean('status')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_orders');
+    }
+}
